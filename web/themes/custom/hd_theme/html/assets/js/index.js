837304d916e6b118e650ea80b06850c95feb5510
Pashakin <pashakind@gmail.com>
2022-05-13 00:40:55 +0300

    Added burger menu

--- web/themes/custom/hd_theme/html/assets/js/index.js
+++ web/themes/custom/hd_theme/html/assets/js/index.js
@@ -4,4 +4,16 @@
       //code.
     },
   };
+
+  Drupal.behaviors.burgerMenuBehavior = {
+    attach: function (context, settings) {
+      const burgerMenu = document.querySelector(".region-header__burger-menu");
+      const navMenu = document.querySelector(".region-header__navigation-menu");
+
+      burgerMenu.addEventListener("click", function () {
+        burgerMenu.classList.toggle("region-header__burger-menu_active");
+        navMenu.classList.toggle("region-header__navigation-menu_active");
+      });
+    },
+  };
 })(jQuery, Drupal, once);
